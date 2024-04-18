ca47bd36b8b92caf0116a1d626c640a4930d1166
Leo <uncorreoaleo@gmail.com>
2024-04-18 11:30:12 +0300

    Fixed build process

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Lanetalk WebSocket
 
-This project is a React library that facilitates connections to a Centrifuge server using websockets.
+This project is a React hook that facilitates connections to a Centrifuge server using websockets.
 
 Under the hood uses a fork of `centrifuge-js`
+
+### Importing the Library
+
+#### CommonJS (CJS) Format
+To import the library using CommonJS format, you can use the following syntax:
+
+```
+const LanetalkWebsocket = require('lanetalk-websocket');
+```
+
+#### ES Module (ESM) Format
+To import the library using ES Module format, you can use the following syntax:
+```
+import LanetalkWebsocket from 'lanetalk-websocket';
+```
+
+By supporting both CommonJS and ES Module formats, our library can seamlessly integrate into a wide range of JavaScript projects.
---
+++ rollup.config.mjs
@@ -0,0 +1,30 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js', // Assuming package.json has "main": "dist/index.cjs.js"
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js', // Assuming package.json has "module": "dist/index.esm.js"
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(), // Converts CommonJS modules to ES6
+    typescript({ tsconfig: './tsconfig.json' }), // Handles TypeScript
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ],
+};
