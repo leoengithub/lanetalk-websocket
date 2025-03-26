68baf201719ae1dad8cfab8b28f8d672d5abcad1
Leo <leo@Leos-MacBook-Air.local>
2025-03-26 09:19:57 +0300

    feat: add initialization check for type 2 message before processing other messages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import React from 'react';
-import { Centrifuge, PublicationContext, Subscription } from 'lanetalk-centrifuge-js';
+import {
+  Centrifuge,
+  PublicationContext,
+  Subscription,
+} from 'lanetalk-centrifuge-js';
+
+export type RefType = {
+  connection: Centrifuge;
+  subscription: Subscription;
+};
+
+export interface UseCentrifugeProps {
+  url: string;
+  channel: string;
+  apiKey: string;
+  debug?: boolean;
+  ref?: React.Ref<RefType | null>;
+}
 
 const useCentrifuge = ({
   url,
@@ -7,22 +24,11 @@ const useCentrifuge = ({
   apiKey,
   debug = false,
   ref = null,
-}: {
-  url: string;
-  channel: string;
-  apiKey: string;
-  debug?: boolean;
-  ref?: React.Ref<{
-    connection: Centrifuge;
-    subscription: Subscription;
-  } | null>;
-}) => {
+}: UseCentrifugeProps) => {
   const [isConnected, setConnected] = React.useState<boolean>(false);
+  const [isInitialized, setIsInitialized] = React.useState<boolean>(false);
   const [lastMessage, setLastMessage] = React.useState<PublicationContext>();
-  const centrifugeInstance = React.useRef<{
-    connection: Centrifuge;
-    subscription: Subscription;
-  } | null>(null);
+  const centrifugeInstance = React.useRef<RefType | null>(null);
 
   React.useEffect(() => {
     const centrifuge = new Centrifuge(url, {
@@ -56,7 +62,16 @@ const useCentrifuge = ({
     // subscribe to channel and set last message
     subscription
       .on('publication', function (ctx: PublicationContext) {
-        setLastMessage(ctx);
+        // Check if the message is the initialization message (type 2)
+        if (ctx.data?.type === 2) {
+          setIsInitialized(true);
+          return;
+        }
+
+        // Only set messages after initialization
+        if (isInitialized) {
+          setLastMessage(ctx);
+        }
       })
       .subscribe();
 
@@ -70,12 +85,14 @@ const useCentrifuge = ({
       subscription.removeAllListeners();
       centrifuge.disconnect();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url, channel, debug, apiKey]);
 
   React.useImperativeHandle(ref, () => centrifugeInstance.current);
 
   return {
     isConnected,
+    isInitialized,
     lastMessage,
   };
 };
